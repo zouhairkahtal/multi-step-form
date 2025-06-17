cfb7b5d9295cf2aa66aa533fa40ecef56c3d43d9
zouhair <mizouhair4@gmail.com>
2025-06-17 17:35:25 +0200

    ending desktop desing and code

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 function Nav() {
   return (
     <>
@@ -5,9 +7,22 @@ function Nav() {
         <ul className=" w-52 pt-12 pl-8">
           <li className="mb-5">
             <div className="w-full flex  items-center ">
-              <span className="mr-5 w-10 h-10 rounded-full bg-blue200 text-blue950 flex items-center justify-center">
+              <NavLink
+              to={"/your-info"}
+               className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "mr-5 w-10 h-10 rounded-full bg-blue200 text-blue950 flex items-center justify-center"
+                    : "mr-5 w-10 h-10 rounded-full border border-white text-white flex items-center justify-center"
+                }
+ 
+              >
+
+              <span >
                 1
               </span>
+              </NavLink>
               <div>
                 <span className="text-blue300">STEP 1</span>
                 <h2 className="text-white font-bold">YOUR INFO</h2>
@@ -16,9 +31,22 @@ function Nav() {
           </li>
           <li className="mb-5">
             <div className="w-full flex  items-center ">
-              <span className="mr-5 w-10 h-10 rounded-full border border-white text-white flex items-center justify-center">
+              <NavLink
+              to={"/select-plan"}
+               className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "mr-5 w-10 h-10 rounded-full bg-blue200 text-blue950 flex items-center justify-center"
+                    : "mr-5 w-10 h-10 rounded-full border border-white text-white flex items-center justify-center"
+                }
+ 
+              >
+
+              <span >
                 2
               </span>
+              </NavLink>
               <div>
                 <span className="text-blue300">STEP 2</span>
                 <h2 className="text-white font-bold">SELECT PLAN</h2>
@@ -27,9 +55,22 @@ function Nav() {
           </li>
           <li className="mb-5">
             <div className="w-full flex  items-center ">
-              <span className="mr-5 w-10 h-10 rounded-full border border-white text-white flex items-center justify-center">
+              <NavLink
+              to={"/add-ons"}
+               className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "mr-5 w-10 h-10 rounded-full bg-blue200 text-blue950 flex items-center justify-center"
+                    : "mr-5 w-10 h-10 rounded-full border border-white text-white flex items-center justify-center"
+                }
+ 
+              >
+
+              <span >
                 3
               </span>
+              </NavLink>
               <div>
                 <span className="text-blue300">STEP 3</span>
                 <h2 className="text-white font-bold">ADD-ONS</h2>
@@ -38,15 +79,29 @@ function Nav() {
           </li>
           <li className="mb-5">
             <div className="w-full flex  items-center ">
-              <span className="mr-5 w-10 h-10 rounded-full border border-white text-white flex items-center justify-center">
+              <NavLink
+              to={"/summary"}
+               className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "mr-5 w-10 h-10 rounded-full bg-blue200 text-blue950 flex items-center justify-center"
+                    : "mr-5 w-10 h-10 rounded-full border border-white text-white flex items-center justify-center"
+                }
+ 
+              >
+
+              <span >
                 4
               </span>
+              </NavLink>
               <div>
                 <span className="text-blue300">STEP 4</span>
                 <h2 className="text-white font-bold">SUMMARY</h2>
               </div>
             </div>
           </li>
+          
         </ul>
       </div>
     </>
--- src/views/finishing-up.tsx
+++ src/views/finishing-up.tsx
@@ -1,12 +1,17 @@
+import thank from "../assets/images/icon-thank-you.svg";
 
 function FinishingUp() {
-  
-
   return (
     <>
-    <div>
-        
-    </div>
+      <div className="flex flex-col items-center justify-center  w-full h-full">
+        <img src={thank} alt="thank" className="mb-8" />
+        <h1 className="text-3xl font-bold text-blue950 mb-5">Thank you!</h1>
+        <p className="text-Grey500 w-9/12 text-center">
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need support, please feel free to email us
+          at support@loremagaming.com.
+        </p>
+      </div>
     </>
   );
 }
